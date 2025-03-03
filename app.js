9845bdebf08c9defaf767310a616cdc172664821
Slava <vemcenko@gmail.com>
2025-03-03 15:28:38 +0200

    added deployment and nodeport

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const TelegramBot = require("./telegramBot");
 const WebSocketServer = require("./wsServer");
 const dotenv = require("dotenv");
 dotenv.config();
-
+console.log(process.env.TELEGRAM_BOT_TOKEN);
 const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 
 const tableServerTgBot = new TelegramBot.Bot(TOKEN);
